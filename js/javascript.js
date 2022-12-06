414aef05de31d8d2eddeb731083c66d861eaef97
lollomrt <lorenzomartini.mrt@outlook.it>
2022-12-06 17:57:30 +0100

    prova array generazione numeri

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
-function generatoreQuadtati()
+function generatoreQuadrati()
 {
     let elemento = document.createElement("div")
 
@@ -7,10 +7,38 @@ function generatoreQuadtati()
     return elemento
 }
 
+// function numeri (min, max)
+// {
+//     let numList = []
+//     for (let il = 1 il <= 100 il++){
+//         num++
+//     }
+//     return num
+// }
+
 let pulsante = document.getElementById("pulsante").addEventListener ("click", function(){
     let griglia = document.getElementById("griglia")
-    for (let i = 0; i < 100; i++){
-        let quadratoCorrente = generatoreQuadtati()
-        griglia.appendChild(quadratoCorrente)
+    for (let i = 1; i <= 100; i++){
+        let quadrato = generatoreQuadrati()
+        griglia.appendChild(quadrato)
+
+        let numeroConsecutivo = []
+        for (let num = 1; num <= 100; num++){
+            num++
+        }
+        let numero = numeroConsecutivo[num]
+
+        griglia.innerHTML = numero
+        // let numeroConsecutivo = 1
+        // for (let num = 1; num <=100 num++{
+        //     numeroConsecutivo = numeroConsecutivo[num] +1
+        // }
+        // console.log(numeroConsecutivo)
+
+        quadrato.addEventListener("click", function(){
+            this.classList.add("active")
+        })
     }
+
+
 })
